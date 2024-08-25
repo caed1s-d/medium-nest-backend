499e4612a78b2d1309a8b1783dad5564cdc90ceb
caed1s <yura.shennikov05@mail.ru>
2024-08-25 18:43:10 +0300

    add article module

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,6 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 
 @Controller()
 export class ArticleController {
   constructor() {}
+
+  @Post('api/article')
+  async createArticle() {
+    return {
+      code: 0,
+      msg: 'success',
+      data: null,
+    };
+  }
 }
